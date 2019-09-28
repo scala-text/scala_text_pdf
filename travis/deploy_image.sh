5cf841cca17700acf78482e17230cc23fc3a2548
Yoshimura Yuu <yyu@mental.poker>
2019-09-28 18:49:06 +0900

    Do not merge

--- travis/deploy_image.sh
+++ travis/deploy_image.sh
@@ -2,8 +2,8 @@
 
 set -ex
 
-if [[ "${TRAVIS_OS_NAME}" == "linux" && "${TRAVIS_BRANCH}" == "master" && "${TRAVIS_PULL_REQUEST}" == "false" ]]; then
+#if [[ "${TRAVIS_OS_NAME}" == "linux" && "${TRAVIS_BRANCH}" == "master" && "${TRAVIS_PULL_REQUEST}" == "false" ]]; then
   echo "$REGISTRY_PASSWORD" | docker login -u "$REGISTRY_USER" --password-stdin
   docker tag "$IMAGE_NAME" "${IMAGE_NAME}:latest"
   docker push "${IMAGE_NAME}:latest"
-fi
+#fi
