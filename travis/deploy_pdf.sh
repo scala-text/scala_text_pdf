6f6ab3f906521e2fc17ef75a3eb22663329ac785
Yoshimura Yuu <yyu@mental.poker>
2019-09-29 17:37:51 +0900

    Do not merge

--- travis/deploy_pdf.sh
+++ travis/deploy_pdf.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-if [[ "${TRAVIS_OS_NAME}" == "linux" && "${TRAVIS_BRANCH}" == "master" && "${TRAVIS_PULL_REQUEST}" == "false" ]]; then
+#if [[ "${TRAVIS_OS_NAME}" == "linux" && "${TRAVIS_BRANCH}" == "master" && "${TRAVIS_PULL_REQUEST}" == "false" ]]; then
   echo -e "Host github.com\n\tStrictHostKeyChecking no\nIdentityFile ~/.ssh/deploy.key\n" >> ~/.ssh/config
   openssl aes-256-cbc -pass "pass:$SERVER_KEY" -pbkdf2 -in ./travis/deploy_key.enc -d -a -out deploy.key
   cp deploy.key ~/.ssh/
@@ -17,4 +17,4 @@ if [[ "${TRAVIS_OS_NAME}" == "linux" && "${TRAVIS_BRANCH}" == "master" && "${TRA
   sudo git add scala_text.pdf
   sudo git commit -a -m "auto commit on travis $TRAVIS_JOB_NUMBER $TRAVIS_COMMIT"
   git push git@github.com:scalajp/scala_text_pdf.git gh-pages:gh-pages
-fi
+#fi
