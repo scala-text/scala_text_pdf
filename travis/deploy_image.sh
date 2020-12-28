97764ee987e91b201cdbe777daaecaacf3efe042
Yoshimura Yuu <yyu@mental.poker>
2020-12-29 02:16:41 +0900

    Add GitHub Actions

--- travis/deploy_image.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-set -ex
-
-if [[ "${TRAVIS_OS_NAME}" == "linux" && "${TRAVIS_BRANCH}" == "master" && "${TRAVIS_PULL_REQUEST}" == "false" ]]; then
-  docker tag "$IMAGE_NAME" "${IMAGE_NAME}:latest"
-  docker push "${IMAGE_NAME}:latest"
-fi
