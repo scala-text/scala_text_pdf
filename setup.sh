c6eaa07ffcde4103a506c53244e26a6d6f742a27
YOSHIMURA Yuu <yyu@mental.poker>
2020-12-29 14:43:29 +0900

    Update pandoc version

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-set -e
+set -ex
 
 git submodule update --init
 
@@ -13,7 +13,13 @@ cp -r ./scala_text/src/example_projects ./
 for f in ./img/*.svg
 do
   if [[ $f =~ \./img/(.*)\.svg ]]; then
-    inkscape -z -D --file=`pwd`/img/${BASH_REMATCH[1]}.svg --export-pdf=`pwd`/${BASH_REMATCH[1]}.pdf --export-latex=`pwd`/${BASH_REMATCH[1]}.pdf_tex
+    SVG=`pwd`/img/${BASH_REMATCH[1]}.svg
+    PDF=`pwd`/${BASH_REMATCH[1]}.pdf
+    PDFTEX=`pwd`/${BASH_REMATCH[1]}.pdf_tex
+    inkscape -D "$SVG" --export-filename="$PDF" --export-latex
+    PAGES=$(egrep -a '/Type /Page\b' "$PDF" | wc -l | tr -d ' ')
+    python3 ./python/fix_pdf_tex.py "$PAGES" < "$PDFTEX" > "$PDFTEX.tmp"
+    mv "$PDFTEX.tmp" "$PDFTEX"
   fi
 done
 
@@ -25,7 +31,7 @@ for f in ./scala_text/honkit/*.md
 do
   if [[ $f =~ \./scala_text/honkit/(.*)\.md ]]; then
     cp $f ./target/
-    FILENAME="${BASH_REMATCH[1]}.md" pandoc -o "./target/${BASH_REMATCH[1]}.tex" -f markdown_github+footnotes+header_attributes-hard_line_breaks-intraword_underscores --pdf-engine=lualatex --top-level-division=chapter --listings --filter ./filter.py $f
+    FILENAME="${BASH_REMATCH[1]}.md" pandoc -o "./target/${BASH_REMATCH[1]}.tex" -f markdown_github+footnotes+header_attributes-hard_line_breaks-intraword_underscores --pdf-engine=lualatex --top-level-division=chapter --listings --filter ./python/filter.py $f
   fi
 done
 
